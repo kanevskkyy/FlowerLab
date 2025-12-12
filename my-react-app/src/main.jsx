5a38f63987717298e4c60a66d3e703d279015263
Denys <dmelenkoko@gmail.com>
2025-12-12 02:19:16 +0200

    fix

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,10 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// main.jsx або index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
