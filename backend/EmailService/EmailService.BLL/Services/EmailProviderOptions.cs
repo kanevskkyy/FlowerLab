f17981226fd768fd3fe91280d6ebe8df55210521
BohdanBest <bohdanbohdan32@gmail.com>
2026-04-23 23:17:35 +0300

    Replace SendGrid with Gmail SMTP for email delivery

--- backend/EmailService/EmailService.BLL/Services/EmailProviderOptions.cs
+++ backend/EmailService/EmailService.BLL/Services/EmailProviderOptions.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EmailService.BLL.Service
 {
     public class EmailProviderOptions
     {
-        public string? ApiKey { get; set; }
         public string? FromEmail { get; set; }
         public string? FromName { get; set; }
+        public string SmtpHost { get; set; } = "smtp.gmail.com";
+        public int SmtpPort { get; set; } = 587;
+        public string? SmtpPassword { get; set; }
     }
 }
